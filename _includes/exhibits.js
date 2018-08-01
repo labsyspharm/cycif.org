7c9fd8da61ff3fd651ccc93ad7190f4a19012fa2
yu-anchen <atwood12@gmail.com>
2018-08-01 09:08:28 -0400

    Minor UI adjustment for xs screen

--- _includes/exhibits.js
+++ _includes/exhibits.js
@@ -1269,7 +1269,7 @@ HashState.prototype = {
   addGroup: function(group, g) {
     var aEl = document.createElement('a');
     aEl = Object.assign(aEl, {
-      className: this.g === g ? 'nav-link active' : 'nav-link',
+      className: this.g === g ? 'nav-link btn-primary active' : 'nav-link btn-primary',
       href: 'javascript:;',
       innerText: group.Name,
       title: group.Path,
