2bb41a9b47400de048d0e83d32d514f5aae2a14e
John Hoffer <thejohnhoffer@users.noreply.github.com>
2020-03-25 13:35:58 -0400

    AWS polly takes configurable bucket parameter (#73)

--- scripts/run_polly.py
+++ scripts/run_polly.py
@@ -4,25 +4,26 @@ import botocore
 import boto3
 import yaml
 import sys
+import argparse
 from bs4 import BeautifulSoup
 from markdown import markdown
 
-def upload_hash(text_md, text_key):
+def upload_hash(text_md, text_key, bucket):
     polly_client = boto3.client('polly')
     text_html = BeautifulSoup(markdown(text_md), features="html.parser")
     text = ''.join(text_html.findAll(text=True))
     response = polly_client.synthesize_speech(Text=text, OutputFormat="mp3", VoiceId="Matthew")
     audio = response['AudioStream'].read()
     s3_client = boto3.client("s3")
-    s3_client.put_object(ACL="public-read", Body=audio, Bucket="www.cycif.org", ContentType="audio/mpeg", StorageClass="REDUCED_REDUNDANCY", Key=f"speech/{text_key}.mp3")
+    s3_client.put_object(ACL="public-read", Body=audio, Bucket=bucket, ContentType="audio/mpeg", StorageClass="REDUCED_REDUNDANCY", Key=f"speech/{text_key}.mp3")
 
-def delete_hash(text_key):
+def delete_hash(text_key, bucket):
     s3_client = boto3.client("s3")
-    s3_client.delete_object(Bucket="www.cycif.org", Key=f"speech/{text_key}.mp3")
+    s3_client.delete_object(Bucket=bucket, Key=f"speech/{text_key}.mp3")
 
-def list_hash():
+def list_hash(bucket):
     s3_client = boto3.client("s3")
-    response = s3_client.list_objects_v2(Bucket="www.cycif.org", Prefix=f"speech/")
+    response = s3_client.list_objects_v2(Bucket=bucket, Prefix=f"speech/")
     contents = response.get('Contents', None)
     if contents:
         return [c['Key'].split('/')[1].split('.')[0] for c in contents]
@@ -48,24 +49,29 @@ def yield_texts(data_path):
                         yield w['Description']
 
 if __name__ == "__main__":
+  
+    parser = argparse.ArgumentParser()
+    parser.add_argument("bucket")
+    args = parser.parse_args()
+    bucket = args.bucket
 
     try:
-        a = list_hash()
+        list_hash(bucket)
     except botocore.exceptions.NoCredentialsError as e:
         print('No Available AWS Credentials')
         sys.exit(0)
-        
+ 
     root = pathlib.Path(__file__).resolve().parents[1]
     texts = [t for t in yield_texts(root / "_data")]
     sha1_texts = {do_sha1(t):t for t in texts} 
 
     needed_sha1 = set(sha1_texts.keys())
-    existing_sha1 = set(list_hash())
+    existing_sha1 = set(list_hash(bucket))
 
     for h in needed_sha1 - existing_sha1:
-        upload_hash(sha1_texts[h], h)
+        upload_hash(sha1_texts[h], h, bucket)
         print(f'uploaded {h}')
     for h in existing_sha1 - needed_sha1:
-        delete_hash(h)
+        delete_hash(h, bucket)
         print(f'deleted {h}')
     
